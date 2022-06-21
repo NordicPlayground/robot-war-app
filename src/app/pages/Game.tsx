293784781bc6a719ef981eff4ab256e3403edb54
Semikaja <kaja.koren@nordicsemi.no>
2022-06-21 13:47:11 +0200

    feat: add input for robot movement
    input fields added, updates the angle and drivetime values in the game module as well

--- src/app/pages/Game.tsx
+++ src/app/pages/Game.tsx
@@ -1,8 +1,9 @@
+import style from 'app/pages/Game.module.css'
 import { Field } from 'components/Game/Field'
+import { InputField } from 'components/Game/InputField'
 import { Robot } from 'components/Game/Robot'
 import { nanoid } from 'nanoid'
 import React, { useState } from 'react'
-import style from './Game.module.css'
 
 const randomColor = () =>
 	`#${Math.floor(Math.random() * 16777215)
@@ -23,48 +24,79 @@ export const Game = () => {
 			yMm: number
 			colorHex: string
 			rotationDeg: number
+			drivetime: number
 		}[]
 	>([])
+	console.log(robots)
 
 	return (
-		<div className={style.field}>
-			<Field
-				heightMm={fieldHeightMm}
-				widthMm={fieldWidthMm}
-				numberOfHelperLines={3}
-				startZoneSizeMm={startZoneSizeMm}
-				onClick={({ xMm, yMm }) =>
-					setRobots((robots) => [
-						...robots,
-						{ xMm, yMm, id: nanoid(), colorHex: randomColor(), rotationDeg: 0 },
-					])
-				}
-			>
-				{robots.map(({ xMm, yMm, id, colorHex, rotationDeg }) => (
-					<Robot
-						key={id}
-						id={id}
-						xMm={xMm}
-						yMm={yMm}
-						widthMm={robotWidthMM}
-						heightMm={robotLengthMm}
-						colorHex={colorHex}
-						rotationDeg={rotationDeg}
-						onRotate={(rotation) => {
-							setRobots((robots) => [
-								...robots.filter(({ id: robotId }) => robotId !== id),
-								{
-									xMm,
-									yMm,
-									id,
-									colorHex,
-									rotationDeg: rotationDeg + rotation,
-								},
-							])
-						}}
-					/>
-				))}
-			</Field>
+		<div>
+			<div className={style.field}>
+				<Field
+					heightMm={fieldHeightMm}
+					widthMm={fieldWidthMm}
+					numberOfHelperLines={3}
+					startZoneSizeMm={startZoneSizeMm}
+					onClick={({ xMm, yMm }) =>
+						setRobots((robots) => [
+							...robots,
+							{
+								xMm,
+								yMm,
+								id: nanoid(),
+								colorHex: randomColor(),
+								rotationDeg: 90,
+								drivetime: 0,
+							},
+						])
+					}
+				>
+					{robots.map(({ xMm, yMm, id, colorHex, rotationDeg }) => (
+						<Robot
+							key={id}
+							id={id}
+							xMm={xMm}
+							yMm={yMm}
+							widthMm={robotWidthMM}
+							heightMm={robotLengthMm}
+							colorHex={colorHex}
+							rotationDeg={rotationDeg}
+							onRotate={(rotation) => {
+								setRobots((robots) => [
+									...robots.filter(({ id: robotId }) => robotId !== id),
+									{
+										xMm,
+										yMm,
+										id,
+										colorHex,
+										rotationDeg: rotationDeg + rotation,
+										drivetime: 0,
+									},
+								])
+							}}
+						/>
+					))}
+				</Field>
+			</div>
+			{robots.map(({ xMm, yMm, id, colorHex, rotationDeg }) => (
+				<InputField
+					key={id}
+					id={id}
+					onpress={(rotation, duration) => {
+						setRobots((robots) => [
+							...robots.filter(({ id: robotId }) => robotId !== id),
+							{
+								xMm,
+								yMm,
+								id,
+								colorHex,
+								rotationDeg: rotationDeg + rotation,
+								drivetime: duration,
+							},
+						])
+					}}
+				></InputField>
+			))}
 		</div>
 	)
 }
