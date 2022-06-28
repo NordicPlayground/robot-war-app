3eabae5bd8334696545943c3134bc156fac67a06
Semikaja <kaja.koren@nordicsemi.no>
2022-06-28 10:21:53 +0200

    fix: include context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { App } from 'app/App'
 import { CredentialsProvider } from 'hooks/useCredentials'
+import { GameAdminProvider } from 'hooks/useGameAdmin'
 import { GameControllerProvider } from 'hooks/useGameController'
 import { GameControllerThingProvider } from 'hooks/useGameControllerThing'
 import { createRoot } from 'react-dom/client'
@@ -9,9 +10,11 @@ const root = createRoot(container)
 root.render(
 	<GameControllerThingProvider>
 		<CredentialsProvider>
-			<GameControllerProvider>
-				<App />
-			</GameControllerProvider>
+			<GameAdminProvider>
+				<GameControllerProvider>
+					<App />
+				</GameControllerProvider>
+			</GameAdminProvider>
 		</CredentialsProvider>
 	</GameControllerThingProvider>,
 )
