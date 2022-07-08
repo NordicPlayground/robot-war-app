f8d53bfa584223d46f6d9fe6327c9548d4ed791a
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2022-07-08 10:59:22 +0200

    fix: use admin shadow to list 'select team' options (#28)
    Other changes includes changing the name of a component from 'Modal' to 'Popup' and from 'SelectTeam' to 'TeamSelectionUser'.
    
    Co-authored-by: Kaja Koren <kaja.koren@nordicsemi.no>
    Co-authored-by: Mauro Lopez <mauro.lopez@nordicsemi.no>

--- src/components/Game/TeamSelectionUser.tsx
+++ src/components/Game/TeamSelectionUser.tsx
@@ -1,5 +1,6 @@
-import { Modal } from 'components/Modal'
-import styles from 'components/Modal.module.css'
+import { Popup } from 'components/Popup'
+import styles from 'components/Popup.module.css'
+import { useGameAdmin } from 'hooks/useGameAdmin'
 import { useGameController } from 'hooks/useGameController'
 import { useTimer } from 'hooks/useTimer'
 import { useState } from 'react'
@@ -7,9 +8,12 @@ import { useState } from 'react'
 /*
  ** Select the team from the user perspective
  */
-export const SelectTeam = () => {
-	const { teamNameOptions, setSelectedTeam, selectedTeam } = useGameController()
+export const TeamSelectionUser = () => {
+	const { setSelectedTeam } = useGameController()
 	const [isTeamSelected, setIsTeamSelected] = useState<boolean>(false)
+	const {
+		metaData: { robotTeamAssignment },
+	} = useGameAdmin()
 	// TODO: set team name on welcome message
 	// const [teamName, setTeamName] = useState<string|undefined>(undefined)
 	const [seconds, start, reset] = useTimer()
@@ -19,6 +23,9 @@ export const SelectTeam = () => {
 		start(3)
 		setIsTeamSelected(true)
 	}
+	const teamNameArray = Object.values(robotTeamAssignment).filter(
+		(item, i, ar) => ar.indexOf(item) === i,
+	)
 
 	if (seconds < 0) {
 		reset()
@@ -26,7 +33,7 @@ export const SelectTeam = () => {
 
 	if (!isTeamSelected) {
 		return (
-			<Modal>
+			<Popup>
 				<h1 className={styles.title}>SELECT YOUR TEAM</h1>
 				<select
 					className="form-select"
@@ -36,22 +43,20 @@ export const SelectTeam = () => {
 					}}
 				>
 					<option value="0">Select team</option>
-					{teamNameOptions.map(
-						(team: Record<string, string>, index: number) => (
-							<option key={index} value={index + 1}>
-								{team.name}
-							</option>
-						),
-					)}
+					{teamNameArray.map((team: string, index: number) => (
+						<option key={index} value={team}>
+							{team}
+						</option>
+					))}
 				</select>
-			</Modal>
+			</Popup>
 		)
 	}
 
 	return (
 		<>
 			{seconds > 0 ? (
-				<Modal>
+				<Popup>
 					<h1 className={styles.title}>WELCOME!</h1>
 					{/* 
 						TODO: set team name on welcome message
@@ -62,7 +67,7 @@ export const SelectTeam = () => {
 						{' '}
 						You are gonna be redirected to the game in {seconds} seconds{' '}
 					</h2>
-				</Modal>
+				</Popup>
 			) : null}
 		</>
 	)
