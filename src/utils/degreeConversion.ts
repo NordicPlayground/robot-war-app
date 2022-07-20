5050271cd20247d38f5aa4646218887e5e396395
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-07-20 12:51:18 +0200

    docs: add documentation to degreeConversion method

--- src/utils/degreeConversion.ts
+++ src/utils/degreeConversion.ts
@@ -1,3 +1,6 @@
+/**
+ * Convert a degree from 180 format to 360
+ */
 export const degreeConversion = (degree: number): number => {
 	if (degree > 180 || degree < -180)
 		throw new Error(`Degree is out of range: ${degree}.`)
