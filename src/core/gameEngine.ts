d78f9e6ec07a4d3d0ab9be742754199bd4492d8c
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-07-20 09:45:19 +0200

    refactor: rename method BREAKING CHANGE: 'adminSetRobotPositions' is now 'adminSetRobotsPosition'

--- src/core/gameEngine.ts
+++ src/core/gameEngine.ts
@@ -76,7 +76,7 @@ export type GameEngine = {
 	/**
 	 * Used by the Admin to set the position and rotation on the field for all robots at once
 	 */
-	adminSetRobotPositions: (
+	adminSetRobotsPosition: (
 		positions: Record<Static<typeof MacAddress>, Static<typeof Position>>,
 	) => void
 	/**
@@ -270,7 +270,7 @@ export const gameEngine = ({
 				position: positionUpdate,
 			})
 		},
-		adminSetRobotPositions: (positions) => {
+		adminSetRobotsPosition: (positions) => {
 			Object.entries(positions).forEach(([robotAddress, position]) => {
 				updatePosition(robotAddress, position)
 				updateRotationDeg(robotAddress, position)
