a2324e489efa97f38bb6c5132f729775aeffc8b0
Markus Tacker <m@coderbyheart.com>
2022-07-21 15:36:24 +0200

    feat(admin): connect view to core

--- src/app/pages/GameEngineDemo.tsx
+++ src/app/pages/GameEngineDemo.tsx
@@ -1,12 +1,10 @@
 import { randomMac } from 'core/test/randomMac.js'
 import { randomRobot } from 'core/test/randomRobot'
 import { useCore } from 'hooks/useCore'
-import { useState } from 'react'
 
 export const GameEngineDemo = () => {
 	const {
-		game: { adminAssignRobotToTeam, gatewayReportDiscoveredRobots },
-		robots,
+		game: { gatewayReportDiscoveredRobots },
 	} = useCore()
 
 	return (
@@ -30,19 +28,7 @@ export const GameEngineDemo = () => {
 				</button>
 			</p>
 
-			<ul>
-				{Object.entries(robots).map(([address, robot]) => (
-					<li key={address}>
-						{address}
-						<RobotTeam
-							address={address}
-							onSave={(teamName) => {
-								adminAssignRobotToTeam(address, teamName)
-							}}
-						/>
-					</li>
-				))}
-			</ul>
+			<RobotsList />
 
 			<TeamsList />
 		</>
@@ -61,32 +47,14 @@ const TeamsList = () => {
 	)
 }
 
-const RobotTeam = ({
-	address,
-	onSave,
-}: {
-	address: string
-	onSave: (teamName: string) => void
-}) => {
-	const [teamName, setTeamName] = useState<string>('')
+const RobotsList = () => {
+	const { robots } = useCore()
 
 	return (
-		<>
-			<label htmlFor={`robot-${address}-name`}>Team</label>
-			<input
-				type={'text'}
-				id={`robot-${address}-name`}
-				value={teamName}
-				onChange={(e) => setTeamName(e.target.value)}
-			/>
-			<button
-				type={'button'}
-				onClick={() => {
-					onSave(teamName)
-				}}
-			>
-				save
-			</button>
-		</>
+		<ul>
+			{Object.entries(robots).map(([mac, robot]) => (
+				<li key={mac}>{mac}</li>
+			))}
+		</ul>
 	)
 }
---
+++ src/components/Admin/RobotTeamAssigner.tsx
@@ -0,0 +1,83 @@
+import { useCore } from 'hooks/useCore'
+import { useState } from 'react'
+
+export const RobotTeamAssigner = () => {
+	const {
+		game: { adminAssignRobotToTeam },
+		robots,
+	} = useCore()
+
+	if (Object.keys(robots).length === 0) return null
+
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault()
+			}}
+		>
+			<fieldset>
+				<legend>Assign robots to teams </legend>
+				<ul>
+					{Object.entries(robots).map(([address, robot]) => (
+						<li key={address}>
+							{address}
+							<RobotTeam
+								address={address}
+								team={robot.team}
+								onSave={(teamName) => {
+									console.log(teamName)
+									adminAssignRobotToTeam(address, teamName)
+								}}
+							/>
+						</li>
+					))}
+				</ul>
+			</fieldset>
+		</form>
+	)
+}
+
+const RobotTeam = ({
+	address,
+	team,
+	onSave,
+}: {
+	address: string
+	team?: string
+	onSave: (teamName: string) => void
+}) => {
+	const { teams } = useCore()
+	const [teamName, setTeamName] = useState<string>(team ?? '')
+
+	const isValidName = () => teamName.length > 0
+
+	return (
+		<>
+			<label htmlFor={`robot-${address}-name`}>Team</label>
+			<input
+				type={'text'}
+				id={`robot-${address}-name`}
+				value={teamName}
+				onChange={(e) => setTeamName(e.target.value)}
+				className={'form-control'}
+				onBlur={() => {
+					if (isValidName()) onSave(teamName)
+				}}
+				placeholder={'e.g. "Gear Grinders"'}
+			/>
+			{teams.map((team) => (
+				<button
+					key={team}
+					className={'btn btn-secondary'}
+					title={`Assign the robot ${address} to the team ${team}...`}
+					onClick={() => {
+						onSave(team)
+						setTeamName(team)
+					}}
+				>
+					{team}
+				</button>
+			))}
+		</>
+	)
+}
