d1521e6e5802028bfe48562cd4996f6ad327f4c4
Markus Tacker <m@coderbyheart.com>
2022-07-24 21:37:38 +0200

    feat(core-component): make number of robots configurable

--- src/app/pages/GameEngine.tsx
+++ src/app/pages/GameEngine.tsx
@@ -2,33 +2,51 @@ import { Main } from 'components/Main'
 import { randomMac } from 'core/test/randomMac.js'
 import { randomRobot } from 'core/test/randomRobot'
 import { useCore } from 'hooks/useCore'
+import { useState } from 'react'
 
 export const GameEngine = () => {
 	const {
 		game: { gatewayReportDiscoveredRobots },
 	} = useCore()
 
+	const [nRobots, setNRobots] = useState<number>(4)
+
 	return (
 		<Main>
 			<div className="card">
 				<div className="card-header">Core</div>
 				<div className="card-body">
-					<p>
+					<div>
+						<label htmlFor="numberOfRobots" className="form-label">
+							Number of Robots
+						</label>
+						<input
+							type="number"
+							step={1}
+							min={1}
+							className="form-control"
+							id="numberOfRobots"
+							value={nRobots}
+							onChange={(e) => setNRobots(parseInt(e.target.value, 10))}
+						/>
+					</div>
+					<div className="mt-3">
 						<button
 							type="button"
 							className="btn btn-secondary"
 							onClick={() => {
-								gatewayReportDiscoveredRobots({
-									[randomMac()]: randomRobot(),
-									[randomMac()]: randomRobot(),
-									[randomMac()]: randomRobot(),
-									[randomMac()]: randomRobot(),
-								})
+								const robots: Parameters<
+									typeof gatewayReportDiscoveredRobots
+								>[0] = {}
+								for (let i = 0; i < nRobots; i++) {
+									robots[randomMac()] = randomRobot()
+								}
+								gatewayReportDiscoveredRobots(robots)
 							}}
 						>
-							Gateway: Generate Robots
+							Generate {nRobots} Robots
 						</button>
-					</p>
+					</div>
 				</div>
 			</div>
 			<RobotsList />
