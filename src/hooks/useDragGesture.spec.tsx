e7510f62a4efa6946fec88352f7b98eb17d2cf99
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-07-25 16:16:16 +0200

    test(usedraggesture): test subtract coordinates function

--- src/hooks/useDragGesture.spec.tsx
+++ src/hooks/useDragGesture.spec.tsx
@@ -1,4 +1,4 @@
-import { result } from 'hooks/useDragGesture.js'
+import { result, subtractCoordinates } from 'hooks/useDragGesture.js'
 
 describe('UseRobotActionGesture', () => {
 	it('given 2 coordinates should return the rotation degree and distance in pixeles between them', () => {
@@ -48,5 +48,22 @@ describe('UseRobotActionGesture', () => {
 				expect(distancePx).toBeGreaterThanOrEqual(0)
 			},
 		)
+	})
+
+	describe('subtractCoordinates', () => {
+		test('Given 2 coordinates should return the subtract of both, creating a new coordinate', () => {
+			const referencePoint: [number, number] = [10, 10]
+			const desiredPoint: [number, number] = [50, 50]
+			const [x, y] = subtractCoordinates(referencePoint, desiredPoint)
+			expect(x).not.toBeNaN()
+			expect(y).not.toBeNaN()
+		})
+		test('Given 2 coordinates should subtract the second coordinate over the first coordinate passed by params', () => {
+			const referencePoint: [number, number] = [10, 10]
+			const desiredPoint: [number, number] = [50, 50]
+			const [x, y] = subtractCoordinates(referencePoint, desiredPoint)
+			expect(x).toEqual(-40)
+			expect(y).toEqual(-40)
+		})
 	})
 })
