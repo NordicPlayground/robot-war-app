194cd192b81b27263180ecc056748d1c6a76c99b
Kaja Koren <107171119+Semikaja@users.noreply.github.com>
2022-07-28 14:46:48 +0200

    fix: fixed number of colors to teams, not random anymore (#46)
    This provides a pre-selected list of colors for teams so they are easier to distinguish.
    
    Co-authored-by: Mauro López Jiménez <Mauro.Lopez@NordicSemi.no>
    Co-authored-by: Markus Tacker <Markus.Tacker@NordicSemi.no>

--- src/utils/teamColor.spec.ts
+++ src/utils/teamColor.spec.ts
@@ -1,21 +1,32 @@
 import { teamColor } from 'utils/teamColor.js'
 
+const colorRx = /#[a-f0-9]{6}/
+
 describe('teamColor()', () => {
 	const t1 = 'Titans'
 	const t2 = 'Gear Grinders'
 
-	it('should return hex colors', () =>
-		expect(teamColor(t1)).toMatch(/#[a-f0-9]{6}/))
+	it('should return hex colors', () => expect(teamColor(t1)).toMatch(colorRx))
 
 	it('should provide stable team colors', () => {
 		expect(teamColor(t2)).toEqual(teamColor(t2))
 		expect(teamColor(t1)).toEqual(teamColor(t1))
 	})
 
-	it('should provide different colors per team', () =>
-		expect(teamColor(t1)).not.toEqual(teamColor(t2)))
+	it('should provide different colors to the per team, except when there is no more colors to choose from', () => {
+		const colors = ['#ffbe0b', '#8338ec']
 
-	it('should provide a default color', () =>
+		const team1Color = teamColor(t1, colors)
+		const team2Color = teamColor(t2, colors)
+		expect(team1Color).not.toEqual(team2Color)
+
+		// If we run out of colors, the last color is used
+		const team3Color = teamColor('Team 3', colors)
+		expect(team3Color).toMatch(colorRx)
+		expect(team3Color).toEqual(colors[colors.length - 1])
+	})
+
+	it('should provide a defaults color', () =>
 		expect(teamColor()).toMatch(/#[a-f0-9]{6}/))
 
 	it('should provide the same default color', () =>
--- src/utils/teamColor.ts
+++ src/utils/teamColor.ts
@@ -1,13 +1,36 @@
-import { randomColor } from 'utils/randomColor.js'
-
 export const defaultColor = '#333333'
 const teamColors: Record<string, string> = {}
 
+const teamColorOption = [
+	'#ffbe0b',
+	'#8338ec',
+	'#fb5607',
+	'#3a86ff',
+	'#ff006e',
+	'#00f5d4',
+	'#00bbf9',
+	'#ffe440',
+	'#9b5de5',
+	'#f15bb5',
+]
+
 /**
- * Provide stable colors for teams
+ * Provide stable colors for teams.
+ *
+ * If there are not enough colors, the last one in the list is used.
  */
-export const teamColor = (team?: string): string => {
+export const teamColor = (
+	team?: string,
+	availableColors = teamColorOption,
+): string => {
 	if (team === undefined) return defaultColor
-	if (teamColors[team] === undefined) teamColors[team] = randomColor()
+	if (teamColors[team] === undefined) {
+		const pickedColors = Object.values(teamColors)
+		const unpickedColor = availableColors.find(
+			(color) => !pickedColors.includes(color),
+		)
+		teamColors[team] =
+			unpickedColor ?? availableColors[availableColors.length - 1]
+	}
 	return teamColors[team]
 }
