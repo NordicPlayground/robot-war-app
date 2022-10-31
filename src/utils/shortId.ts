2add7c2472bc1a8f42eceb2d92109e7c10b5924b
Kaja Koren <107171119+Semikaja@users.noreply.github.com>
2022-10-31 14:37:02 +0100

    Update src/utils/shortId.ts
    Co-authored-by: Markus Tacker <m@coderbyheart.com>

--- src/utils/shortId.ts
+++ src/utils/shortId.ts
@@ -1,4 +1,5 @@
-/**This is needed, because we want to reduce the id/MAC-adress displayed for the
+/**
+ * This is needed, because we want to reduce the id/MAC-adress displayed for the
  * robots to a given number that should be equal everywhere
  */
 export const shortId = (id: string): string => id.slice(0, 3)
