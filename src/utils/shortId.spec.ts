574a2131209901a50ea9c60ad63f5ae56bcaf95a
Kaja Koren <kaja.koren@nordicsemi.no>
2022-10-31 14:40:10 +0100

    fix: updating tests to cover more scenarios

--- src/utils/shortId.spec.ts
+++ src/utils/shortId.spec.ts
@@ -3,10 +3,10 @@ import { shortId } from 'utils/shortId.js'
 describe('shortId()', () => {
 	describe('it should reduce the string input to only the three first characters in the string ', () => {
 		it.each([
-			['Example', 'Exa'],
+			['15e638e5d725', '15e'],
 			['Test', 'Tes'],
 			['robotId', 'rob'],
-			['Hello', 'Hel'],
+			['He', 'He'],
 		])('"%s" -> "%s"', (received, expected) =>
 			expect(shortId(received)).toBe(expected),
 		)
